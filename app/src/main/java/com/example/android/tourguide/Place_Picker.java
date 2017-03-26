2c4db89fe4d8703aea20edf7037a942db5da7958
Learn French <mansi.jain.kota@gmail.com>
2017-03-26 12:22:41 +0530

    Displaying information from PlacePicker

--- app/src/main/java/com/example/android/tourguide/Place_Picker.java
+++ app/src/main/java/com/example/android/tourguide/Place_Picker.java
@@ -2,11 +2,14 @@ package com.example.android.tourguide;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -17,6 +20,7 @@ import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.text.Text;
 
 import static android.os.Build.VERSION_CODES.M;
 import static com.example.android.tourguide.R.id.imageView;
@@ -24,20 +28,25 @@ import static com.example.android.tourguide.SignIn.REQ_CODE;
 
 public class Place_Picker extends AppCompatActivity {
 
-    private TextView txt,name;
+    private TextView name,type,priceLevel;
+    private TextView address1,phoneNo;
     private final int REQ_CODE = 1 ;
     private ImageView imageView1;
-
+    private TextView website;
     private RatingBar rate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place__picker);
-        txt = (TextView)findViewById(R.id.place);
+        address1 = (TextView) findViewById(R.id.place);
         rate = (RatingBar) findViewById(R.id.ratingBar);
-
+        type = (TextView) findViewById(R.id.type);
+        website = (TextView) findViewById(R.id.website);
         name = (TextView) findViewById(R.id.name12);
+        phoneNo = (TextView) findViewById(R.id.phone);
         imageView1 = (ImageView) findViewById(R.id.imagedart);
+        priceLevel = (TextView) findViewById(R.id.price);
         imageView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,6 +54,8 @@ public class Place_Picker extends AppCompatActivity {
                 startPlacePickerActivity();
             }
         });
+
+
     }
 
 
@@ -73,12 +84,36 @@ public class Place_Picker extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode==REQ_CODE && resultCode== Activity.RESULT_OK){
             Place place = PlacePicker.getPlace(this,data);
+            imageView1.setVisibility(View.GONE);
             String address = place.getAddress().toString();
             String name13 = place.getName().toString();
-            imageView1.setVisibility(View.GONE);
-            txt.setText(address);
-            rate.setRating(place.getRating());
+
             name.setText(name13);
+            address1.setText(address);
+            phoneNo.setText(place.getPhoneNumber());
+            rate.setRating(place.getRating());
+
+           //Toast.makeText(getApplicationContext(),"mansi"+place.getRating(),Toast.LENGTH_LONG).show();
+           rate.setEnabled(false);
+            Uri url = place.getWebsiteUri();
+
+           website.setText(url.toString());
+
+            switch (place.getPriceLevel())
+            {
+                case 0 : priceLevel.setText("Cheapest");
+                    break;
+                case 1 :priceLevel.setText("Cheaper");
+                    break;
+                case 2 :priceLevel.setText("Moderate");
+                    break;
+                case 3 :priceLevel.setText("Expensive");
+                    break;
+                case 4 :priceLevel.setText("Most Expensive");
+                    break;
+
+            }
+
         }
 
 
