bc567c7543be98478f5e87a4ccc94946e4cdec98
Learn French <mansi.jain.kota@gmail.com>
2017-04-02 17:51:07 +0530

    DatePicker and TimePicker Implemented

--- app/src/main/java/com/example/android/tourguide/CreateTrip.java
+++ app/src/main/java/com/example/android/tourguide/CreateTrip.java
@@ -2,12 +2,14 @@ package com.example.android.tourguide;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
@@ -16,12 +18,12 @@ import java.util.Calendar;
 
 public class CreateTrip extends AppCompatActivity {
 
-    private TextView checkInDate , checkOutDate,name,address;
+    private TextView checkInDate , checkOutDate,name,address,checkInTime,checkOutTime;
     int year,month,day;
     private Calendar calendar;
-    private static final int ID=999,REQ=1000;
+    private static final int ID=999,REQ=1000,ID_1=1001,REQ_1=1002;
     int year1,day1,month1;
-
+    int hour,minute,hour1,minute1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +34,10 @@ public class CreateTrip extends AppCompatActivity {
         checkInDate.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
         checkOutDate = (TextView) findViewById(R.id.CheckOutDate);
         checkOutDate.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        checkInTime = (TextView) findViewById(R.id.CheckInTime);
+        checkInTime.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        checkOutTime = (TextView) findViewById(R.id.CheckOutTime);
+        checkOutTime.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
         calendar = Calendar.getInstance();
         year = calendar.get(Calendar.YEAR);
         name = (TextView) findViewById(R.id.nameOfPlace);
@@ -43,13 +49,17 @@ public class CreateTrip extends AppCompatActivity {
         year1 = calendar.get(Calendar.YEAR);
         month1 = calendar.get(Calendar.MONTH);
         day1 = calendar.get(Calendar.DAY_OF_MONTH);
+        hour = calendar.get(Calendar.HOUR_OF_DAY);
+        minute = calendar.get(Calendar.MINUTE);
+        hour1 = calendar.get(Calendar.HOUR_OF_DAY);
+        minute1 = calendar.get(Calendar.MINUTE);
         showDate(year, month+1, day);
-
         showDate1(year1,month1+1,day1);
+        showTime(hour,minute);
+        showTime1(hour1,minute1);
     }
 
     private void showDate1(int year1, int month1, int day1) {
-        Toast.makeText(getApplicationContext(),"bbbbbbbbbbbbbbbbb",Toast.LENGTH_LONG);
         checkOutDate.setText(new StringBuilder().append(day1).append("/")
                 .append(month1).append("/").append(year1));
     }
@@ -65,8 +75,13 @@ public class CreateTrip extends AppCompatActivity {
                     myDateListener, year, month, day);
         }
         if (id == REQ){
-            Toast.makeText(getApplicationContext(),"cccccccccccccccc",Toast.LENGTH_LONG);
             return new DatePickerDialog(this,myDateListener1,year,month,day);
+        }
+        if (id == ID_1){
+            return new TimePickerDialog(this,myTimeListener,hour,minute,false);
+        }
+        if (id == REQ_1){
+            return new TimePickerDialog(this,myTimeListener1,hour,minute,false);
         }
         return null;
     }
@@ -99,8 +114,79 @@ public class CreateTrip extends AppCompatActivity {
 
     public void setCheckOutDate(View view){
         showDialog(REQ);
-        Toast.makeText(getApplicationContext(),"aaaaaaaaaaaaaaa",Toast.LENGTH_LONG);
     }
 
 
+    public void setCheckInTime(View view){
+        showDialog(ID_1);
+    }
+
+    public void setCheckOutTime(View view){
+        showDialog(REQ_1);
+    }
+
+
+
+
+    private TimePickerDialog.OnTimeSetListener myTimeListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            showTime(hourOfDay,minute);
+        }
+    };
+
+    private TimePickerDialog.OnTimeSetListener myTimeListener1 = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            showTime1(hourOfDay,minute);
+        }
+    };
+
+    public void showTime(int hour,int minute){
+        String format;
+        if (hour == 00) {
+            hour += 12;
+            format = "AM";
+        } else if (hour == 12) {
+            format = "PM";
+        } else if (hour > 12) {
+            hour -= 12;
+            format = "PM";
+        } else {
+            format = "AM";
+        }
+
+        if(minute < 10){
+            checkInTime.setText(new StringBuilder().append(hour).append(":").append(0)
+                    .append(minute).append(format));
+        }
+        else {
+            checkInTime.setText(new StringBuilder().append(hour).append(":")
+                    .append(minute).append(format));
+        }
+    }
+
+    public void showTime1(int hour,int minute){
+        String format;
+        if (hour == 00) {
+            hour += 12;
+            format = "AM";
+        } else if (hour == 12) {
+            format = "PM";
+        } else if (hour > 12) {
+            hour -= 12;
+            format = "PM";
+        } else {
+            format = "AM";
+        }
+
+        if(minute < 10){
+            checkOutTime.setText(new StringBuilder().append(hour).append(":").append(0)
+                    .append(minute).append(format));
+        }
+        else {
+            checkOutTime.setText(new StringBuilder().append(hour).append(":")
+                    .append(minute).append(format));
+        }
+    }
 }
